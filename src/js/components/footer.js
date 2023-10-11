d45ac29a5e035ddbb408de9adabdc0b992fa1b37
webtoma <thomas.delalbre@gmail.com>
2023-10-11 12:30:33 +0200

    modified footer for netlify test

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -5,7 +5,7 @@ class Footer extends HTMLElement {
         this.innerHTML = `
         <footer>    
             <div class="container">
-                footer
+                footer 2023
             </div>
         </footer>`;
     }
